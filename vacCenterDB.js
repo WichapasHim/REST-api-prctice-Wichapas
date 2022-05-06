e4b299dcfd34f782c429c90362faa4d4dbfe4790
WichapasHim <86400398+WichapasHim@users.noreply.github.com>
2022-05-06 12:12:02 +0700

    Delete vacCenterDB.js

--- vacCenterDB.js
+++
@@ -1,9 +0,0 @@
-const mysql = require("mysql");
-var connection = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "0867644280",
-  database: "vacCenter"
-});
-
-module.exports = connection;
