012e637446f09d99a7528fd0445c46e6f55d00d1
WichapasHim <86400398+WichapasHim@users.noreply.github.com>
2022-05-06 12:10:51 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,32 +1,81 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
+const cookieParser = require("cookie-parser");
+const mongoSanitizer = require('express-mongo-sanitize');
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+
 
 //Load env vars
 dotenv.config({ path: "./config/config.env" });
 
 //Routes files
 const hospitals = require("./routes/hospitals");
-const appointments = require("../routes/appointments");
+const appointments = require("./routes/appointments");
 const auth = require("./routes/auth");
 const app = express();
 //connect to database
 connectDB();
 
+const swaggerOptions ={
+  swaggerDefinition:{
+    openapi:'3.0.0',
+    info:{
+      title: 'Library API',
+      version: '1.0.0',
+      description: 'A simple Express VacQ API'
+    },
+    servers: [
+      {
+        url: 'http://localhost:5000/api/v1'
+      }
+    ],
+  },
+  apis:['./routes/*.js'],
+};
+const swaggerJsDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swaggerJsDocs));
+
 //body parser
 app.use(express.json());
-
-//Cookie parser
+// Cookie parser
 app.use(cookieParser());
+//sanitize
+app.use(mongoSanitizer());
 
-app.use("/api/v1/auth", auth);
+//srt security header
+app.use(helmet());
+
+//prevent xss attacks
+app.use(xss());
+
+//Rate Limiting
+const limiter =  rateLimit({
+  windowsMs : 10*60*1000 ,// 10 mins
+  max:100
+})
+app.use(limiter);
+
+//prevent http param pollutions
+app.use(hpp());
+
+//enable CORS
+app.use(cors());
+
+
+
 
 //Mount routers
 app.use("/api/v1/hospitals", hospitals);
+app.use("/api/v1/auth", auth);
 app.use("/api/v1/appointments", appointments);
-
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
@@ -39,3 +88,5 @@ process.on("unhandleRejection", (err, Promise) => {
   //close server and exit process
   server.close(() => process.exit(1));
 });
+
+
